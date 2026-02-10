afc47559aac6ea5d48a35e53d19e10eade8892ab
marinaoleinik <marina.oleinik@tthk.ee>
2026-02-10 11:20:17 +0200

    Add project files.

---
+++ Naidis_TARpv24/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Naidis_TARpv24
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("luffio Reguler 400.ttf", "LuffioRegular");
+                    fonts.AddFont("luffio.ttf", "Luffio");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
